aa1f2063eb5e66bd09d760555fafa9e8fd045941
Kit <akuma548@gmail.com>
2022-10-03 19:33:10 -0700

    Initial commit

---
+++ test/slack-integration-v2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SlackIntegrationV2 from '../lib/slack-integration-v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/slack-integration-v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SlackIntegrationV2.SlackIntegrationV2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
